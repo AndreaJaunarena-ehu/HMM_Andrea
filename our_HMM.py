312bd5839d106470feb13a2f47a4813d827864d2
AndreaJaunarena-ehu <andreajaunarena22@gmail.com>
2024-11-30 22:22:25 +0100

    first commit

---
+++ our_HMM.py
@@ -0,0 +1,13 @@
+class our_HMM:
+
+    # A set of N states Q = q1, q2, ..., qN
+    # A sequence of T observations O = o1, o2, ..., oN 
+    # A transition probability matrix A = a11, ..., aij, aNN
+    # A sequence of observation likelihoods B = bi(ot)
+    # A initial probability distribution over states π = π1, π2, ..., πN
+
+    def __init__(self) -> None:
+        pass
+
+    def viterbi_algorithm():
+
